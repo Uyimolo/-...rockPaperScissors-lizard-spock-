77e8cb7a1595ac79acceeaee2a5b87e0cc9414a3
Urhoghide Justin Uyioghosa <uyijustindavid@gmail.com>
2024-03-16 10:46:01 +0100

    added score refresh, save to local storage to make game score persist on browser refresh and added comments to important sections of the app for future use

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import ScoreHeader from './components/ScoreHeader';
-import MainGameArea from './components/MoveSelection';
 import GameLogic from './components/GameLogic';
 import Rules from './components/Rules';
+import MoveSelection from './components/MoveSelection';
 
 function App() {
   const [showRules, setShowRules] = useState(false);
-  const [score, setScore] = useState(0);
+
+  // declare score useState with a callback function to retreive score from local storage if its present
+  const [score, setScore] = useState(() => {
+    const localScore = localStorage.getItem('score');
+    return localScore ? parseInt(localScore) : 0;
+  });
   const [playerMove, setPlayerMove] = useState('');
+
+  // update store in local storage when ever score is updated in app
+  useEffect(() => {
+    localStorage.setItem('score', score.toString());
+  }, [score]);
 
   return (
     <div className='app'>
-      <ScoreHeader score={score} />
+      <ScoreHeader
+        score={score}
+        setScore={setScore}
+        setPlayerMove={setPlayerMove}
+      />
+
+      {/* show gamelogic component once playerMove is set (player makes a move)  */}
 
       {!playerMove ? (
-        <MainGameArea setPlayerMove={setPlayerMove} />
+        <MoveSelection setPlayerMove={setPlayerMove} />
       ) : (
         <GameLogic
           playerMove={playerMove}
--- src/components/GameLogic.jsx
+++ src/components/GameLogic.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 import movesArr from '../arrays/movesArr';
 import ChallengeMove from './ChallengeMove';
@@ -13,18 +13,21 @@ const GameLogic = ({ playerMove, setPlayerMove, setScore }) => {
     handleDominance();
   }, [theHouseMove]);
 
+  // delay complayer move to alloy the shuffle animation in placeholder div take place
   useEffect(() => {
-    const suspense = setTimeout(() => {
+    const comPlayerDelay = setTimeout(() => {
       handleRandomHouseMove();
     }, 2000);
-    return () => clearTimeout(suspense);
+    return () => clearTimeout(comPlayerDelay);
   }, [playerMove]);
 
+  // get complayers move randomly
   function handleRandomHouseMove() {
     const randomIndex = Math.floor(Math.random() * movesArr.length);
     setTheHouseMove(movesArr[randomIndex]);
   }
 
+  // main game logic: determine if its a win by checking if the player move dominates the computers move
   function handleDominance() {
     if (theHouseMove) {
       if (
@@ -47,16 +50,21 @@ const GameLogic = ({ playerMove, setPlayerMove, setScore }) => {
       <div className='game-logic-container'>
         <div className='challengers'>
           <div className='player'>
+            {/* show player move */}
             <ChallengeMove
               img={playerGameMove[0].img}
               name={playerGameMove[0].name}
             />
             <h2>YOU PICKED</h2>
           </div>
-          { gameResult && <div className='result-play-again'>
-            <h1>{gameResult.toUpperCase()}</h1>
-            <button onClick={() => setPlayerMove('')}>PLAY AGAIN</button>
-          </div>}
+          {gameResult && (
+            <div className='result-play-again'>
+              <h1>{gameResult.toUpperCase()}</h1>
+              <button onClick={() => setPlayerMove('')}>PLAY AGAIN</button>
+            </div>
+          )}
+
+          {/* show computers game move after delay (placeholder div) */}
           <div className='com-player player'>
             {theHouseMove ? (
               <ChallengeMove img={theHouseMove.img} name={theHouseMove.name} />
